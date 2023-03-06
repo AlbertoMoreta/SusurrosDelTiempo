2f0d46da3a1622cd02e2b9e020701855bb4739ac
Alberto Moreta <albertomoreta70@gmail.com>
2023-03-06 01:53:06 +0100

    Change DialogBox height

--- Assets/Scripts/Dialogs/DialogManager.cs
+++ Assets/Scripts/Dialogs/DialogManager.cs
@@ -15,6 +15,8 @@ public class DialogManager : MonoBehaviour {
     public GameObject dialogBox;
     public float dialogBoxOffset = 1f;
     public TextMeshProUGUI diaryNotes;
+    public float dialogBoxHeight = 1f;
+
     private TextMeshProUGUI _subsTextBox;
     
     private Transform _originalParent;
@@ -90,7 +92,7 @@ public class DialogManager : MonoBehaviour {
                     break;
                 }
             }
-            dialogBox.transform.position = character.transform.position + new Vector3(xOffset, 0.7f, 0);
+            dialogBox.transform.position = character.transform.position + new Vector3(xOffset, dialogBoxHeight, 0);
             _subsTextBox.text = sub.text;
             // if(sub.hint){
                 diaryNotes.text += "- " + "hola\n";
